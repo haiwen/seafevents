c71b6f3243dedb8fc1908cc682af6acb381fa72a
lins05 <linshuai2012@gmail.com>
2013-05-18 19:01:31 +0800

    improve index.py

--- index.py
+++ index.py
@@ -6,6 +6,54 @@ import time
 import subprocess
 import logging
 
+def find_in_path(prog):
+    if 'win32' in sys.platform:
+        sep = ';'
+    else:
+        sep = ':'
+
+    dirs = os.environ['PATH'].split(sep)
+    for d in dirs:
+        d = d.strip()
+        if d == '':
+            continue
+        path = os.path.join(d, prog)
+        if os.path.exists(path):
+            return path
+
+    return None
+
+def _get_python_executable():
+    if sys.executable and os.path.isabs(sys.executable) and os.path.exists(sys.executable):
+        return sys.executable
+
+    try_list = [
+        'python2.7',
+        'python27',
+        'python2.6',
+        'python26',
+    ]
+
+    for prog in try_list:
+        path = find_in_path(prog)
+        if path is not None:
+            return path
+
+    path = os.environ.get('PYTHON', 'python')
+
+    return path
+
+pyexec = None
+def get_python_executable():
+    '''Find a suitable python executable'''
+    global pyexec
+    if pyexec is not None:
+        return pyexec
+
+    pyexec = _get_python_executable()
+    return pyexec
+
+
 def run(argv, cwd=None, env=None, suppress_stdout=False, suppress_stderr=False):
     '''Run a program and wait it to finish, and return its exit code. The
     standard output of this program is supressed.
@@ -36,6 +84,7 @@ def update_file_index(seafesdir, logfile):
     loglevel = 'debug'
     # python update_repos.py --logfile ./index.log --loglevel debug update
     cmd = [
+        get_python_executable(),
         script_path,
         '--logfile', logfile,
         '--loglevel', loglevel,
