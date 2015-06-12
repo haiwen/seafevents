a181a2592d65d089ebb2ea9db76ce8184770e228
llj <lingjun.li1@gmail.com>
2015-06-12 19:19:03 +0800

    [office preview] modification for UI, zoom in/out

--- office_converter/pdf2htmlEX/iframe.js
+++ office_converter/pdf2htmlEX/iframe.js
@@ -1,23 +1,18 @@
-var console = {
-    panel: $(parent.document.body).append('<div>'),
-    log: function(m){
-        this.panel.prepend('<div>'+m+'</div>');
-    }       
+var jquery_file = document.getElementById('jquery');
+jquery_file.src = window.parent.document.body.getElementsByTagName('script')[0].src;
+jquery_file.onload = function () {
+    $(window).on('message', function(event) {
+        var e = event.originalEvent;
+        if (e.data && e.data.scale) {
+            var scale = e.data.scale;
+            $('body').css({
+                'transform':'scale(' + scale + ')',
+                '-webkit-transform':'scale(' + scale + ')',
+                '-ms-transform':'scale(' + scale + ')',
+                'transform-origin':'top left',
+                '-webkit-transform-origin':'top left',
+                '-ms-transform-origin':'top left'
+            });
+        }
+    });
 };
-
-$(window).on('message', function(event) {
-    var e = event.originalEvent;
-    if (e.data && e.data.scale) {
-        var scale = e.data.scale;
-        // console.log('scale to ' + scale);
-        $('.pf').css({
-            'transform':'scale(' + scale + ')',
-            '-webkit-transform':'scale(' + scale + ')',
-            '-ms-transform':'scale(' + scale + ')',
-            'transform-origin':'top left',
-            '-webkit-transform-origin':'top left',
-            '-ms-transform-origin':'top left'
-        });
-        $('#page-container').css({'overflow':'hidden', 'background':'none'});
-    }
-});
