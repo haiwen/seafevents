360bd3cebd2954fe0eb7cd19435400a567774136
zming <517046497@qq.com>
2018-03-26 22:31:03 +0800

    [fixed] change the way that events of change file path use mysql

--- events/change_file_path.py
+++ events/change_file_path.py
@@ -3,30 +3,29 @@
 import os
 import logging
 import hashlib
-import MySQLdb
+
+from sqlalchemy.sql import text
+from seafevents.db import create_mysql_engine
+from sqlalchemy.orm.scoping import scoped_session
 
 
 class ChangeFilePathHandler(object):
     def __init__(self):
         self.db_conn = None
-        self.cursor = None
 
         self.init_seahub_db()
-
-        if self.cursor is None:
-            logging.debug('Failed to init seahub db.')
 
     def init_seahub_db(self):
         try:
             import seahub_settings
-            #import local_settings
+            # import local_settings
         except ImportError as e:
-            logging.warning('Failed to init seahub db: %s.' %  e)  
+            logging.warning('Failed to init seahub db: %s.' %  e)
             return
 
         try:
             db_infos = seahub_settings.DATABASES['default']
-            #db_infos = local_settings.DATABASES['default']
+            # db_infos = local_settings.DATABASES['default']
         except KeyError as e:
             logging.warning('Failed to init seahub db, can not find db info in seahub settings.')
             return
@@ -47,61 +46,51 @@ class ChangeFilePathHandler(object):
             return
         self.db_passwd = db_infos.get('PASSWORD')
 
-        try:
-            # type of msg from ccnet is str, so there need str too,
-            # otherwise there will be a conversion operation（ascii is python default encode 
-            # type, it's hard to change）.
-            self.db_conn = MySQLdb.connect(host=self.db_host, port=self.db_port,
-                                           user=self.db_user, passwd=self.db_passwd,
-                                           db=self.db_name, charset='utf8', use_unicode=False)
-            self.db_conn.autocommit(True)
-            self.cursor = self.db_conn.cursor()
-        except Exception as e:
-            logging.warning('Failed to init seahub db: %s.' %  e)
+        self.session = create_mysql_engine(self.db_host, self.db_port,
+                                           self.db_user, self.db_passwd, self.db_name)
 
-    def close_seahub_db(self):
-        if self.cursor:
-            self.cursor.close()
-        if self.db_conn:
-            self.db_conn.close()
+    def get_con(self):
+        return scoped_session(self.session)
 
-    def reconnect_db(self):
-        # If connection is timeout, reconnect
-        try:
-            self.db_conn = MySQLdb.connect(host=self.db_host, port=self.db_port,
-                                       user=self.db_user, passwd=self.db_passwd,
-                                       db=self.db_name, charset='utf8')
-            self.db_conn.autocommit(True)
-            self.cursor = self.db_conn.cursor()
-        except Exception as e:
-            logging.warning('Failed to connect seahub db: %s.' %  e)
+    def trans_to_unicode(self, str_list):
+        return [ e.decode('utf-8') for e in str_list]
+
 
     def update_db_records(self, dst_repo_id, path, new_path, is_dir, src_repo_id=None):
         if not dst_repo_id or not path or not new_path:
             logging.warning('Failed to change file uuid map, bad args')
             return
 
+        dst_repo_id, path, new_path = self.trans_to_unicode([dst_repo_id, path, new_path])
+        if src_repo_id:
+            src_repo_id = self.trans_to_unicode([src_repo_id])
         self.change_file_uuid_map(dst_repo_id, path, new_path, is_dir, src_repo_id)
         self.change_share_file_path(dst_repo_id, path, new_path, is_dir, src_repo_id)
         self.change_upload_share_file_path(dst_repo_id, path, new_path, is_dir, src_repo_id)
 
     def change_share_file_path(self, repo_id, path, new_path, is_dir, src_repo_id=None):
         try:
-            self._change_share_file_path(repo_id, path, new_path, is_dir, src_repo_id)
-        except MySQLdb.OperationalError:
-            self.reconnect_db()
-            self._change_share_file_path(repo_id, path, new_path, is_dir, src_repo_id)
+            session = self.get_con()
+            thread_local_session = session()
+            self._change_share_file_path(thread_local_session, repo_id, path, new_path, is_dir, src_repo_id)
+        except Exception as e:
+            session = self.get_con()
+            thread_local_session = session()
+            self._change_share_file_path(thread_local_session, repo_id, path, new_path, is_dir, src_repo_id)
         except Exception as e:
             logging.warning('Failed to change share file path for repo %s, path:%s, new_path: %s, %s.' % (repo_id, path, new_path, e))
+        finally:
+            session.remove()
 
-    def _change_share_file_path(self, repo_id, path, new_path, is_dir, src_repo_id=None):
-        self.cursor.execute('select path from share_fileshare where repo_id=%s and path like %s',
-                            [src_repo_id if src_repo_id else repo_id, path + '%'])
-        if self.cursor.rowcount == 0:
+    def _change_share_file_path(self, session, repo_id, path, new_path, is_dir, src_repo_id=None):
+        result = session.execute(text('select path from share_fileshare where repo_id=:repo_id and path like :path')
+                                 , {'repo_id': src_repo_id if src_repo_id else repo_id, 'path': path + '%'})
+        if result.rowcount == 0:
             return
+
         # For multi-layer dirs, divide orig_path into orig_parent_path and orig_sub_path
         # new_path_value = new_path + orig_sub_path
-        results = self.cursor.fetchall()
+        results = result.fetchall()
         # get all records that path starts with old path
         # e.g
         # old_path: /old_path/t
@@ -109,7 +98,7 @@ class ChangeFilePathHandler(object):
         for row in results:
             # row[0]: old path in db
             # path: old path
-            # new_path_value: new path 
+            # new_path_value: new path
 
             # pass only oldpath and subdir path
             if row[0] == path or row[0].startswith(path + '/'):
@@ -118,48 +107,55 @@ class ChangeFilePathHandler(object):
                 else:
                     new_path_value = new_path + row[0].split(path, 1)[1]
 
-            # update old path and subdir record
-                self.cursor.execute('''update share_fileshare set repo_id=%s, path=%s
-                                where repo_id=%s and path=%s''',
-                                (repo_id, new_path_value,
-                                src_repo_id if src_repo_id else repo_id, row[0]))
+                # update old path and subdir record
+                session.execute(text('update share_fileshare set repo_id=:new_repo_id, path=:new_path '
+                                     'where repo_id=:old_repo_id and path=:old_path'),
+                                {'new_repo_id': repo_id, 'new_path': new_path_value,
+                                 'old_repo_id': src_repo_id if src_repo_id else repo_id, 'old_path': row[0]})
+                session.commit()
 
     def change_file_uuid_map(self, repo_id, path, new_path, is_dir, src_repo_id=None):
         try:
-            self._change_file_uuid_map (repo_id, path, new_path, is_dir, src_repo_id)
-        except MySQLdb.OperationalError:
-            self.reconnect_db()
-            self._change_file_uuid_map (repo_id, path, new_path, is_dir, src_repo_id)
+            session = self.get_con()
+            thread_local_session = session()
+            self._change_file_uuid_map(thread_local_session, repo_id, path, new_path, is_dir, src_repo_id)
+        except Exception:
+            session = self.get_con()
+            thread_local_session = session()
+            self._change_file_uuid_map(thread_local_session, repo_id, path, new_path, is_dir, src_repo_id)
         except Exception as e:
             logging.warning('Failed to change file uuid map for repo %s, path:%s, new_path: %s, %s.' % (repo_id, path, new_path, e))
+        finally:
+            session.remove()
 
-    def _change_file_uuid_map (self, repo_id, path, new_path, is_dir, src_repo_id = None):
+    def _change_file_uuid_map(self, session, repo_id, path, new_path, is_dir, src_repo_id = None):
         old_dir = os.path.split(path)[0]
         old_file = os.path.split(path)[1]
-        self.cursor.execute('select 1 from tags_fileuuidmap where repo_id=%s and parent_path=%s and filename=%s and is_dir=%s',
-                            [src_repo_id if src_repo_id else repo_id, old_dir, old_file, is_dir])
+        result = session.execute(text('select 1 from tags_fileuuidmap where repo_id=:repo_id and parent_path=:parent_path and filename=:filename and is_dir=:is_dir'),
+                                 {'repo_id': src_repo_id if src_repo_id else repo_id, 'parent_path': old_dir, 'filename': old_file, 'is_dir': is_dir})
         # update old path itself
-        if self.cursor.rowcount != 0:
+        if result.rowcount != 0:
             new_dir = os.path.split(new_path)[0]
             new_file = os.path.split(new_path)[1]
 
             path_md5 = self.md5_repo_id_parent_path(repo_id, new_dir)
 
-            self.cursor.execute('''update tags_fileuuidmap set repo_id=%s, parent_path=%s, filename=%s,
-                            repo_id_parent_path_md5=%s where 
-                            repo_id=%s and parent_path=%s and filename=%s and is_dir=%s''',
-                            (repo_id, new_dir, new_file, path_md5,
-                            src_repo_id if src_repo_id else repo_id, old_dir, old_file, is_dir))
+            session.execute(text('update tags_fileuuidmap set repo_id=:new_repo_id, parent_path=:new_dir, filename=:new_file, '
+                                 'repo_id_parent_path_md5=:new_md5 where '
+                                 'repo_id=:repo_id and parent_path=:dir and filename=:filename and is_dir=:is_dir'),
+                            {'new_repo_id': repo_id, 'new_dir': new_dir, 'new_file': new_file, 'new_md5': path_md5,
+                             'repo_id': src_repo_id if src_repo_id else repo_id, 'dir': old_dir, 'filename': old_file, 'is_dir': is_dir})
+            session.commit()
 
         # sub-dir #
         if is_dir:
-            self.cursor.execute ('select parent_path from tags_fileuuidmap where repo_id=%s and parent_path like %s',
-                                [src_repo_id if src_repo_id else repo_id, path + '%'])
-            if self.cursor.rowcount == 0:
+            result = session.execute(text('select parent_path from tags_fileuuidmap where repo_id=:repo_id and parent_path like :path'),
+                                     {'repo_id':src_repo_id if src_repo_id else repo_id, 'path': path + '%'})
+            if result.rowcount == 0:
                 return
             # For multi-layer dirs, divide orig_path into orig_parent_path and orig_sub_path
             # new_path_value = new_path + orig_sub_path
-            results = self.cursor.fetchall()
+            results = result.fetchall()
             # get all record that parent_path starts with old parent_path
             # e.g
             # old_path: /old_path/t
@@ -177,35 +173,40 @@ class ChangeFilePathHandler(object):
                         new_path_value = new_path + row[0].split(path, 1)[1]
                     path_md5 = self.md5_repo_id_parent_path(repo_id, new_path_value)
 
-                    self.cursor.execute('''update tags_fileuuidmap set repo_id=%s, parent_path=%s,
-                                    repo_id_parent_path_md5=%s where
-                                    repo_id=%s and parent_path=%s''',
-                                    (repo_id, new_path_value, path_md5,
-                                    src_repo_id if src_repo_id else repo_id, row[0]))
+                    session.execute(text('update tags_fileuuidmap set repo_id=:new_repo_id, parent_path=:new_dir, '
+                                         'repo_id_parent_path_md5=:new_md5 where '
+                                         'repo_id=:repo_id and parent_path=:dir'),
+                                    {'new_repo_id': repo_id, 'new_dir': new_path_value, 'new_md5': path_md5,
+                                     'repo_id': src_repo_id if src_repo_id else repo_id, 'dir': row[0]})
+                    session.commit()
 
     def md5_repo_id_parent_path(self, repo_id, parent_path):
-        parent_path = parent_path.rstrip('/') if parent_path != '/' else '/' 
-        # repo_id and parent_path are already utf8 encoded characters, can't use encode
-        return hashlib.md5(repo_id + parent_path).hexdigest()
+        parent_path = parent_path.rstrip('/') if parent_path != '/' else '/'
+        return hashlib.md5((repo_id + parent_path).encode('utf-8')).hexdigest().decode('utf-8')
 
     def change_upload_share_file_path(self, repo_id, path, new_path, is_dir, src_repo_id=None):
         try:
-            self._change_upload_share_file_path(repo_id, path, new_path, is_dir, src_repo_id)
-        except MySQLdb.OperationalError:
-            self.reconnect_db()
-            self._change_upload_share_file_path(repo_id, path, new_path, is_dir, src_repo_id)
+            session = self.get_con()
+            thread_local_session = session()
+            self._change_upload_share_file_path(thread_local_session, repo_id, path, new_path, is_dir, src_repo_id)
+        except Exception:
+            session = self.get_con()
+            thread_local_session = session()
+            self._change_upload_share_file_path(thread_local_session, repo_id, path, new_path, is_dir, src_repo_id)
         except Exception as e:
             logging.warning('Failed to change upload share file path for repo %s, path:%s, new_path: %s, %s.' % (repo_id, path, new_path, e))
+        finally:
+            session.remove()
 
-    def _change_upload_share_file_path(self, repo_id, path, new_path, is_dir, src_repo_id):
-        self.cursor.execute('select path from share_uploadlinkshare where repo_id=%s and path like %s',
-                            [src_repo_id if src_repo_id else repo_id, path + '%'])
+    def _change_upload_share_file_path(self, session, repo_id, path, new_path, is_dir, src_repo_id):
+        result = session.execute(text('select path from share_uploadlinkshare where repo_id=:repo_id and path like :dir'),
+                                {'repo_id': src_repo_id if src_repo_id else repo_id, 'dir': path + '%'})
 
-        if self.cursor.rowcount == 0:
+        if result.rowcount == 0:
             return
         # For multi-layer dirs, divide orig_path into orig_parent_path and orig_sub_path
         # new_path_value = new_path + orig_sub_path
-        results = self.cursor.fetchall()
+        results = result.fetchall()
         # get all records that path starts with old path
         # e.g
         # old_path: /old_path/t
@@ -213,7 +214,7 @@ class ChangeFilePathHandler(object):
         for row in results:
             # row[0]: old path in db
             # path: old path
-            # new_path_value: new path 
+            # new_path_value: new path
 
             # pass only oldpath and subdir path
             if row[0] == path or row[0].startswith(path + '/'):
@@ -223,7 +224,8 @@ class ChangeFilePathHandler(object):
                     new_path_value = new_path + row[0].split(path, 1)[1]
 
             # update old path and subdir record
-                self.cursor.execute('''update share_uploadlinkshare set repo_id=%s, path=%s
-                                where repo_id=%s and path=%s''',
-                                (repo_id, new_path_value,
-                                src_repo_id if src_repo_id else repo_id, row[0]))
+            session.execute(text('update share_uploadlinkshare set repo_id=:new_repo_id, path=:new_path '
+                                 'where repo_id=:repo_id and path=:path'),
+                            {'new_repo_id': repo_id, 'new_path': new_path_value,
+                             'repo_id': src_repo_id if src_repo_id else repo_id, 'path': row[0]})
+            session.commit()
