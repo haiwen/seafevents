e443e807523fad7dfabdac5faf753fed7649848b
zming <517046497@qq.com>
2018-05-12 15:39:58 +0800

    get_related_users_by_org_repo -> get_shared_users_by_repo get_related_users_by_org_repo -> org_get_shared_users_by_repo

--- events/handlers.py
+++ events/handlers.py
@@ -5,12 +5,10 @@ import logging
 import logging.handlers
 import datetime
 
-from seaserv import get_related_users_by_repo, get_org_id_by_repo_id, \
-    get_related_users_by_org_repo
+from seaserv import get_org_id_by_repo_id, seafile_api
 from seafevents.events.db import save_user_events, save_org_user_events, \
         save_file_audit_event, save_file_update_event, save_perm_audit_event
-from seafevents.app.config import appconfig
-from seafobj import CommitDiffer, commit_mgr
+from seafobj import commit_mgr
 from change_file_path import ChangeFilePathHandler
 
 changer = ChangeFilePathHandler()
@@ -54,10 +52,13 @@ def RepoUpdateEventHandler(session, msg):
 
     org_id = get_org_id_by_repo_id(repo_id)
     if org_id > 0:
-        users = get_related_users_by_org_repo(org_id, repo_id)
+        users_obj = seafile_api.org_get_shared_users_by_repo(org_id, repo_id)
+        owner = seafile_api.get_org_repo_owner(repo_id)
     else:
-        users = get_related_users_by_repo(repo_id)
+        users_obj = seafile_api.get_shared_users_by_repo(repo_id)
+        owner = seafile_api.get_repo_owner(repo_id)
 
+    users = [e.user for e in users_obj] + [owner]
     if not users:
         return
 
@@ -78,7 +79,7 @@ def RepoTrashEventHandler(session, msg):
     OBJ_FILE = 'file'
     OBJ_DIR = 'dir'
     if etype == 'clean-up-repo-trash':
-        repo_id, repo_name, operator, days, date, is_dir= elements[1:]
+        repo_id, repo_name, operator, days, date, is_dir = elements[1:]
         detail = {
             'repo_id': repo_id,
             'days': days,
@@ -139,7 +140,7 @@ def FileUpdateEventHandler(session, msg):
 
     time = datetime.datetime.utcfromtimestamp(msg.ctime)
 
-    save_file_update_event(session, time, commit.creator_name, org_id, \
+    save_file_update_event(session, time, commit.creator_name, org_id,
                            repo_id, commit_id, commit.description)
 
 def FileAuditEventHandler(session, msg):
@@ -158,7 +159,7 @@ def FileAuditEventHandler(session, msg):
 
     org_id = get_org_id_by_repo_id(repo_id)
 
-    save_file_audit_event(session, timestamp, msg_type, user_name, ip, \
+    save_file_audit_event(session, timestamp, msg_type, user_name, ip,
                           user_agent, org_id, repo_id, file_path)
 
 def PermAuditEventHandler(session, msg):
@@ -177,11 +178,11 @@ def PermAuditEventHandler(session, msg):
 
     org_id = get_org_id_by_repo_id(repo_id)
 
-    save_perm_audit_event(session, timestamp, etype, from_user, to, \
+    save_perm_audit_event(session, timestamp, etype, from_user, to,
                           org_id, repo_id, file_path, perm)
 
-def send_to_ali_mq(added_files, deleted_files, added_dirs, deleted_dirs, modified_files, \
-                   renamed_files, moved_files, renamed_dirs, moved_dirs, commit, \
+def send_to_ali_mq(added_files, deleted_files, added_dirs, deleted_dirs, modified_files,
+                   renamed_files, moved_files, renamed_dirs, moved_dirs, commit,
                    commit_id, repo_id, parent):
     from seafevents.events.alimq_producer import ali_mq
 
