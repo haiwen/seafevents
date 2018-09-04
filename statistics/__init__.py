3a562c6117826343b7dc75470b1b7ebfaff289fa
cuihaikuo <haikuo.cui@seafile.com>
2018-09-04 18:33:38 +0800

    Support file type statistics.

--- statistics/__init__.py
+++ statistics/__init__.py
@@ -1,5 +1,6 @@
 from .total_storage import TotalStorageCounter
 from .file_ops import FileOpsCounter
+from .file_types import FileTypesCounter
 from .models import TotalStorageStat
 from .models import FileOpsStat
 from .stat_settings import Settings
