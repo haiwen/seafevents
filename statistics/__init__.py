5ee82e68fb413113d6590be5d5982f5d5d1e67d5
ly1217 <yu.liu@seafile.com>
2018-09-05 20:54:41 -0700

    Add history repos storage statistics.

--- statistics/__init__.py
+++ statistics/__init__.py
@@ -1,5 +1,7 @@
 from .total_storage import TotalStorageCounter
+from .history_total_storage import HistoryTotalStorageCounter
 from .file_ops import FileOpsCounter
 from .models import TotalStorageStat
+from .models import HistoryTotalStorageStat
 from .models import FileOpsStat
 from .stat_settings import Settings
