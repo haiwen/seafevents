7b783f51d09a542f94080b67eb47608af04cbaa4
cuihaikuo <haikuo.cui@seafile.com>
2018-09-20 15:45:06 +0800

    Support file type statistics.

--- tasks/statistics.py
+++ tasks/statistics.py
@@ -7,7 +7,7 @@ from sqlalchemy.sql import text
 from sqlalchemy.orm.scoping import scoped_session
 from threading import Thread, Event
 from seafevents.statistics import TotalStorageCounter, FileOpsCounter, TrafficInfoCounter,\
-                                  MonthlyTrafficCounter
+                                  MonthlyTrafficCounter, FileTypesCounter
 from seafevents.statistics.counter import login_records
 from seafevents.app.config import appconfig
 
@@ -26,6 +26,7 @@ class Statistics(Thread):
             CountTotalStorage().start()
             CountFileOps().start()
             CountMonthlyTrafficInfo().start()
+            CountFileTypes().start()
 
 class CountTotalStorage(Thread):
     def __init__(self):
@@ -76,6 +77,18 @@ class CountMonthlyTrafficInfo(Thread):
         while not self.fininsh.is_set():
             MonthlyTrafficCounter().start_count()
             self.fininsh.wait(3600)
+    def cancel(self):
+        self.fininsh.set()
+
+class CountFileTypes(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.fininsh = Event()
+
+    def run(self):
+        while not self.fininsh.is_set():
+            FileTypesCounter().start_count()
+            self.fininsh.wait(appconfig.file_types_interval)
 
     def cancel(self):
         self.fininsh.set()
