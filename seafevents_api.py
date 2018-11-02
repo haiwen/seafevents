98c7e6ea54cbaf4faeb6cd17e4fc3f1343a5babd
cuihaikuo <haikuo.cui@seafile.com>
2018-11-02 17:07:59 +0800

    Add seafevents_api get_user_activities_by_timestamp().

--- seafevents_api.py
+++ seafevents_api.py
@@ -5,7 +5,7 @@ from .statistics.db import get_file_ops_stats_by_day, get_user_activity_stats_by
      get_all_users_traffic_by_month, get_all_orgs_traffic_by_month, get_user_traffic_by_month,\
      get_org_storage_stats_by_day, get_org_file_ops_stats_by_day, get_org_user_activity_stats_by_day
 
-from .events.db import get_new_file_path
+from .events.db import get_new_file_path, get_user_activities_by_timestamp
 
 def init(config_file):
     if not appconfig.get('session_cls'):
