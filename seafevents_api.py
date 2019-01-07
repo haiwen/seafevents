8b82b0460e3bca475362a4b5f0db6ca907a92809
cuihaikuo <haikuo.cui@seafile.com>
2019-01-07 11:26:10 +0800

    Add seafevents_api.is_pro() and move get_file_history_suffix() to seafevents_api.

--- seafevents_api.py
+++ seafevents_api.py
@@ -10,3 +10,12 @@ from .events.db import get_new_file_path, get_user_activities_by_timestamp
 def init(config_file):
     if not appconfig.get('session_cls'):
         load_config(config_file)
+
+def is_pro():
+    return True
+
+def get_file_history_suffix():
+    if appconfig.fh.enabled is False:
+        return None
+
+    return appconfig.fh.suffix_list
