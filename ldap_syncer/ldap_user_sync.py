93c3407f198ee169ad0b692349ebb855d6884ebe
杨赫然 <heran.yang@seafile.com>
2021-05-13 16:09:27 +0800

    Add record login id to old email

--- ldap_syncer/ldap_user_sync.py
+++ ldap_syncer/ldap_user_sync.py
@@ -597,6 +597,13 @@ class LdapUserSync(LdapSync):
             if ldap_user.dept != db_user.dept or ldap_user.company_code != db_user.company_code:
                 self.update_dept(email, ldap_user.dept, ldap_user.company_code)
 
+    def record_uid_to_old_email (self, uid, old_email):
+        try:
+            self.cursor.execute('insert into login_id_to_old_emails (login_id, old_email) values (%s, %s)',
+                                (uid, old_email))
+        except Exception as e:
+            logger.warning('Failed to insert user %s: %s.' % (uid, old_email))
+
     def sync_migrate_user(self, old_user, new_user):
         if seafile_api.update_email_id (old_user, new_user) < 0:
             logger.warning('Failed to update emailuser id to %s.' % new_user)
@@ -949,6 +956,7 @@ class LdapUserSync(LdapSync):
                         if email != user:
                             del_ldap_user (data_db[user].user_id)
                             logger.debug('Delete user [%s] success.' % user)
+                    self.record_uid_to_old_email (uid, email)
                     self.update_profile_user_login_id (k, uid)
 
         self.close_seahub_db()
