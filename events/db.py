f0e8a9864a3e23bd82022e58f64dbc49030a9da7
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2024-05-23 14:55:22 +0800

    update file access export

--- events/db.py
+++ events/db.py
@@ -9,10 +9,17 @@ from sqlalchemy import desc, select, update, func, text, and_
 from sqlalchemy.sql import exists
 
 from .models import FileAudit, FileUpdate, PermAudit, \
-        Activity, UserActivity, FileHistory
+        Activity, UserActivity, FileHistory, UserLogin
 
+from .utils import write_xls, utc_to_local, generate_file_audit_event_type
+from .task_manager import task_manager
+from seaserv import seafile_api, ccnet_api
+from django.http import HttpResponse, JsonResponse
+from django.utils.translation import gettext as _
+import os
 
 logger = logging.getLogger('seafevents')
+
 
 class UserEventDetail(object):
     """Regular objects which can be used by seahub without worrying about ORM"""
@@ -58,18 +65,18 @@ def _get_user_activities(session, username, start, limit):
     if limit <= 0:
         logger.error('limit must be positive')
         raise RuntimeError('limit must be positive')
-    
-    sub_query = ( 
+
+    sub_query = (
         select(UserActivity.activity_id)
         .where(UserActivity.username == username)
-        
+
     )
     stmt = (
         select(Activity)
         .where(Activity.id.in_(sub_query))
         .order_by(desc(Activity.timestamp))
         .slice(start, start + limit)
-        
+
     )
     events = session.scalars(stmt).all()
 
@@ -81,12 +88,12 @@ def get_user_activities(session, username, start, limit):
 def _get_user_activities_by_timestamp(session, username, start, end):
     events = []
     try:
-        sub_query = ( 
+        sub_query = (
             select(UserActivity.activity_id)
                 .where(
-                    and_(  
-                        UserActivity.username == username,  
-                        UserActivity.timestamp.between(start, end)  
+                    and_(
+                        UserActivity.username == username,
+                        UserActivity.timestamp.between(start, end)
                     )
                 )
         )
@@ -164,7 +171,7 @@ def get_file_history_by_day(session, repo_id, path, start, limit, to_tz, history
     repo_id_path_md5 = hashlib.md5((repo_id + path).encode('utf8')).hexdigest()
     current_item = session.scalars(select(FileHistory).where(FileHistory.repo_id_path_md5 == repo_id_path_md5).
                                    order_by(desc(FileHistory.id)).limit(1)).first()
-    
+
     new_events = []
     if current_item:
         query_stmt = select(
@@ -209,7 +216,7 @@ def get_file_daily_history_detail(session, repo_id, path, start_time, end_time, 
     repo_id_path_md5 = hashlib.md5((repo_id + path).encode('utf8')).hexdigest()
     current_item = session.scalars(select(FileHistory).where(FileHistory.repo_id_path_md5 == repo_id_path_md5).
                                    order_by(desc(FileHistory.id)).limit(1)).first()
-    
+
     details = list()
     try:
         q = select(FileHistory.id, FileHistory.op_type, FileHistory.op_user, FileHistory.timestamp, FileHistory.repo_id, FileHistory.commit_id,
@@ -400,3 +407,210 @@ def get_event_log_by_time(session, log_type, tstart, tend):
     res = session.scalars(stmt).all()
 
     return res
+
+
+def get_sys_logs_task(session, tstart, tend, log_type):
+    if task_manager.tasks_queue.full():
+        logger.warning('seafile io server busy, queue size: %d, current tasks: %s, threads is_alive: %s'
+                                 % (task_manager.tasks_queue.qsize(), task_manager.current_task_info,
+                                    task_manager.threads_is_alive()))
+        return HttpResponse('seafile io server busy.', status=400)
+
+    try:
+        task_id = task_manager.add_export_logs_task(session, tstart, tend, log_type)
+    except Exception as e:
+        logger.error(e)
+        return HttpResponse(e, status=500)
+
+    return task_id
+
+
+def query_status(task_id):
+    if not task_manager.is_valid_task_id(task_id):
+        return HttpResponse('task_id not found.', status=404)
+
+    try:
+        is_finished, error = task_manager.query_status(task_id)
+    except Exception as e:
+        logger.debug(e)
+        return HttpResponse(e, status=500)
+
+    if error:
+        return HttpResponse(error, status=500)
+    return JsonResponse({'is_finished': is_finished})
+
+
+def get_event_log_by_time_to_excel(session, tstart, tend, log_type, task_id):
+
+    if not isinstance(tstart, (int, float)) or not isinstance(tend, (int, float)):
+        logger.error('Invalid time range parameter')
+        raise RuntimeError('Invalid time range parameter')
+
+    if log_type == 'fileaudit':
+        obj = FileAudit
+        stmt = select(obj).where(obj.timestamp.between(datetime.datetime.utcfromtimestamp(tstart),
+                                                       datetime.datetime.utcfromtimestamp(tend)))
+        res = session.scalars(stmt).all()
+
+        head = [_("User"), _("Type"), _("IP"), _("Device"), _("Date"),
+                _("Library Name"), _("Library ID"), _("Library Owner"), _("File Path")]
+        data_list = []
+
+        res.sort(key=lambda x: x.timestamp, reverse=True)
+        for ev in res:
+            event_type, ev.show_device = generate_file_audit_event_type(ev)
+
+            repo_id = ev.repo_id
+            repo = seafile_api.get_repo(repo_id)
+            if repo:
+                repo_name = repo.name
+                repo_owner = seafile_api.get_repo_owner(repo_id) or \
+                             seafile_api.get_org_repo_owner(repo_id)
+            else:
+                repo_name = _('Deleted')
+                repo_owner = '--'
+
+            username = ev.user if ev.user else _('Anonymous User')
+            date = utc_to_local(ev.timestamp).strftime('%Y-%m-%d %H:%M:%S') if \
+                ev.timestamp else ''
+
+            row = [username, event_type, ev.ip, ev.show_device,
+                   date, repo_name, ev.repo_id, repo_owner, ev.file_path]
+            data_list.append(row)
+
+        wb = write_xls('file-access-logs', head, data_list)
+        if not wb:
+            logger.error('Failed to export excel')
+            raise RuntimeError('Failed to export excel')
+
+        target_dir = '/tmp/seafile_events/'
+        os.makedirs(target_dir, exist_ok=True)
+        excel_name = task_id + 'file-access-logs.xlsx'
+        target_path = os.path.join(target_dir, excel_name)
+        wb.save(target_path)
+    elif log_type == 'fileupdate':
+        obj = FileUpdate
+        stmt = select(obj).where(obj.timestamp.between(datetime.datetime.utcfromtimestamp(tstart),
+                                                       datetime.datetime.utcfromtimestamp(tend)))
+        res = session.scalars(stmt).all()
+        head = [_("User"), _("Date"), _("Library Name"), _("Library ID"),
+                _("Library Owner"), _("Action")]
+        data_list = []
+
+        res.sort(key=lambda x: x.timestamp, reverse=True)
+        for ev in res:
+
+            repo_id = ev.repo_id
+            repo = seafile_api.get_repo(repo_id)
+            if repo:
+                repo_name = repo.name
+                repo_owner = seafile_api.get_repo_owner(repo_id) or \
+                             seafile_api.get_org_repo_owner(repo_id)
+            else:
+                repo_name = _('Deleted')
+                repo_owner = '--'
+
+            username = ev.user if ev.user else _('Anonymous User')
+            date = utc_to_local(ev.timestamp).strftime('%Y-%m-%d %H:%M:%S') if \
+                ev.timestamp else ''
+
+            row = [username, date, repo_name, ev.repo_id, repo_owner, ev.file_oper.strip()]
+            data_list.append(row)
+
+        wb = write_xls('file-update-logs', head, data_list)
+        if not wb:
+            logger.error('Failed to export excel')
+            raise RuntimeError('Failed to export excel')
+
+        target_dir = '/tmp/seafile_events/'
+        os.makedirs(target_dir, exist_ok=True)
+        excel_name = task_id + 'file-update-logs.xlsx'
+        target_path = os.path.join(target_dir, excel_name)
+        wb.save(target_path)
+
+    elif log_type == 'permaudit':
+        obj = PermAudit
+        stmt = select(obj).where(obj.timestamp.between(datetime.datetime.utcfromtimestamp(tstart),
+                                                       datetime.datetime.utcfromtimestamp(tend)))
+        res = session.scalars(stmt).all()
+        head = [_("From"), _("To"), _("Action"), _("Permission"), _("Library"),
+                _("Folder Path"), _("Date")]
+        data_list = []
+
+        res.sort(key=lambda x: x.timestamp, reverse=True)
+        for ev in res:
+            repo = seafile_api.get_repo(ev.repo_id)
+            repo_name = repo.repo_name if repo else _('Deleted')
+
+            if '@' in ev.to:
+                to = ev.to
+            elif ev.to.isdigit():
+                group = ccnet_api.get_group(int(ev.to))
+                to = group.group_name if group else _('Deleted')
+            elif 'all' in ev.to:
+                to = _('Organization')
+            else:
+                to = '--'
+
+            if 'add' in ev.etype:
+                action = _('Add')
+            elif 'modify' in ev.etype:
+                action = _('Modify')
+            elif 'delete' in ev.etype:
+                action = _('Delete')
+            else:
+                action = '--'
+
+            if ev.permission == 'rw':
+                permission = _('Read-Write')
+            elif ev.permission == 'r':
+                permission = _('Read-Only')
+            else:
+                permission = '--'
+
+            date = utc_to_local(ev.timestamp).strftime('%Y-%m-%d %H:%M:%S') if \
+                ev.timestamp else ''
+
+            row = [ev.from_user, to, action, permission, repo_name,
+                   ev.file_path, date]
+            data_list.append(row)
+
+        wb = write_xls('perm-audit-logs', head, data_list)
+        if not wb:
+            logger.error('Failed to export excel')
+            raise RuntimeError('Failed to export excel')
+
+        target_dir = '/tmp/seafile_events/'
+        os.makedirs(target_dir, exist_ok=True)
+        excel_name = task_id + 'perm-audit-logs.xlsx'
+        target_path = os.path.join(target_dir, excel_name)
+        wb.save(target_path)
+    elif log_type == 'loginadmin':
+        obj = UserLogin
+        stmt = select(obj).where(obj.login_date.between(datetime.datetime.utcfromtimestamp(tstart),
+                                                       datetime.datetime.utcfromtimestamp(tend)))
+        res = session.scalars(stmt).all()
+
+        head = [_("Name"), _("IP"), _("Status"), _("Time")]
+        data_list = []
+        for log in res:
+            login_time = log.login_date.strftime("%Y-%m-%d %H:%M:%S")
+            status = _('Success') if log.login_success else _('Failed')
+            row = [log.username, log.login_ip, status, login_time]
+            data_list.append(row)
+
+        wb = write_xls('login-logs', head, data_list)
+        if not wb:
+            logger.error('Failed to export excel')
+            raise RuntimeError('Failed to export excel')
+
+        target_dir = '/tmp/seafile_events/'
+        os.makedirs(target_dir, exist_ok=True)
+        excel_name = task_id + 'login-logs.xlsx'
+        target_path = os.path.join(target_dir, excel_name)
+        wb.save(target_path)
+    else:
+        logger.error('Invalid log_type parameter')
+        raise RuntimeError('Invalid log_type parameter')
+
+
