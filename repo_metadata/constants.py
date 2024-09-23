a22370668060c37ede6c369aa74c564a9d7ef3da
杨国璇 <37972689+YangGuoXuan-0503@users.noreply.github.com>
2024-09-23 11:55:34 +0800

    fix: metadata convert property type (#385)
    * fix: metadata convert property type
    
    * metadata add owner
    
    ---------
    
    Co-authored-by: 杨国璇 <ygx@Hello-word.local>
    Co-authored-by: ‘JoinTyang’ <yangtong1009@163.com>

--- repo_metadata/constants.py
+++ repo_metadata/constants.py
@@ -26,6 +26,35 @@ class PropertyTypes:
     GEOLOCATION = 'geolocation'
     BUTTON = 'button'
 
+class PrivatePropertyKeys:
+    ID = '_id'
+    CTIME = '_ctime'
+    MTIME = '_mtime'
+    CREATOR = '_creator'
+    LAST_MODIFIER = '_last_modifier'
+
+    IS_DIR = '_is_dir'
+    PARENT_DIR = '_parent_dir'
+    FILE_CTIME = '_file_ctime'
+    FILE_MTIME = '_file_mtime'
+    FILE_CREATOR = '_file_creator'
+    FILE_MODIFIER = '_file_modifier'
+    FILE_NAME = '_name'
+    FILE_TYPE = '_file_type'
+    FILE_COLLABORATORS = '_collaborators'
+    FILE_EXPIRE_TIME = '_expire_time'
+    FILE_KEYWORDS = '_keywords'
+    FILE_DESCRIPTION = '_description'
+    FILE_EXPIRED = '_expired'
+    FILE_STATUS = '_status'
+    LOCATION = '_location'
+    OBJ_ID = '_obj_id'
+    SIZE = '_size'
+    SUFFIX = '_suffix'
+    FILE_DETAILS = '_file_details'
+    SHOOTING_TIME = '_shooting_time'
+    OWNER = '_owner'
+
 
 class FilterPredicateTypes(object):
     CONTAINS = 'contains'
