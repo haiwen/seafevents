75fb7534e64edac3b73609562a33f6745baecd7b
‘JoinTyang’ <yangtong1009@163.com>
2024-11-01 17:20:42 +0800

    migrate wiki to wiki2

--- seafevent_server/utils.py
+++ seafevent_server/utils.py
@@ -6,14 +6,25 @@ import time
 import openpyxl
 import datetime
 import pytz
+import os
+import json
+import uuid
+import posixpath
+import random
+import hashlib
 
-from sqlalchemy import desc, select
+from sqlalchemy import desc, select, text
 
-from seaserv import seafile_api, ccnet_api
 from seafevents.events.models import FileAudit, FileUpdate, PermAudit, UserLogin
 from seafevents.app.config import TIME_ZONE
 from seafevents.utils.ccnet_db import CcnetDB
 from seafevents.utils.seafile_db import SeafileDB
+from seafevents.repo_metadata.metadata_manager import get_diff_files, ZERO_OBJ_ID
+from seafevents.repo_data import repo_data
+from seafevents.utils.md2sdoc import md2sdoc
+
+from seaserv import get_org_id_by_repo_id, seafile_api, get_commit
+from seafobj import CommitDiffer, commit_mgr, fs_mgr
 
 logger = logging.getLogger('seafevents')
 
@@ -400,3 +411,223 @@ def export_org_event_log_to_excel(session, start_time, end_time, log_type, task_
             excel_name = 'file-access-logs.xlsx'
             target_path = os.path.join(target_dir, excel_name)
             wb.save(target_path)
+
+
+WIKI_PAGES_DIR = '/wiki-pages'
+WIKI_CONFIG_PATH = '_Internal/Wiki'
+WIKI_CONFIG_FILE_NAME = 'index.json'
+FILE_TMP_DIR = '/tmp'
+SYS_DIR_PATHS = ['images']
+
+
+def save_wiki_config(repo_id, username, wiki_config):
+    dir_id = seafile_api.get_dir_id_by_path(repo_id, WIKI_CONFIG_PATH)
+    if not dir_id:
+        seafile_api.mkdir_with_parents(repo_id, '/', WIKI_CONFIG_PATH, username)
+    tmp_content_path = posixpath.join(FILE_TMP_DIR, WIKI_CONFIG_FILE_NAME)
+    with open(tmp_content_path, 'wb') as f:
+        f.write(wiki_config.encode())
+
+    seafile_api.post_file(repo_id, tmp_content_path, WIKI_CONFIG_PATH, WIKI_CONFIG_FILE_NAME, username)
+    os.remove(tmp_content_path)
+
+
+def get_file_content_by_obj_id(repo_id, obj_id):
+    if obj_id == ZERO_OBJ_ID:
+        return ''
+    f = fs_mgr.load_seafile(repo_id, 1, obj_id)
+    b_content = f.get_content().decode()
+    if not b_content.strip():
+        return ''
+
+    return b_content
+
+
+def create_new_wiki_doc_by_old_wiki_path(page_name, file_parent_dir, new_repo_id, file_content, modifier,
+                                         parent_path_to_page_id, pages, navigation, page_id_set, db_session_class,
+                                         old_wiki_path_is_dir):
+
+    sdoc_uuid = uuid.uuid4()
+    sdoc_filename = page_name + '.sdoc'
+    parent_dir = os.path.join(WIKI_PAGES_DIR, str(sdoc_uuid))
+    sdoc_path = os.path.join(parent_dir, sdoc_filename)
+    seafile_api.mkdir_with_parents(new_repo_id, '/', parent_dir.strip('/'), modifier)
+
+    if old_wiki_path_is_dir:
+        seafile_api.post_empty_file(new_repo_id, parent_dir, sdoc_filename, modifier)
+    else:
+        tmp_content_path = posixpath.join(FILE_TMP_DIR, sdoc_filename)
+        with open(tmp_content_path, 'wb') as f:
+            f.write(file_content.encode())
+
+        seafile_api.post_file(new_repo_id, tmp_content_path, parent_dir, sdoc_filename, modifier)
+        os.remove(tmp_content_path)
+
+    values = []
+    file_is_dir = 0
+    parent_path = normalize_path(parent_dir)
+    repo_id_parent_path_md5 = md5_repo_id_parent_path(new_repo_id, parent_path)
+    file_info = (sdoc_uuid.hex, new_repo_id, repo_id_parent_path_md5, parent_dir, sdoc_filename, file_is_dir)
+    values.append(file_info)
+    create_fileuuidmap_by_uuid(db_session_class, values)
+
+    current_page_id = parent_path_to_page_id.get(file_parent_dir)
+    new_page_id = gen_unique_id(page_id_set)
+    page_id_set.add(new_page_id)
+
+    if old_wiki_path_is_dir:
+        tmp_path = posixpath.join(file_parent_dir, page_name)
+        parent_path_to_page_id[tmp_path] = new_page_id
+
+    gen_new_page_nav_by_id(navigation, new_page_id, current_page_id)
+    new_page = {
+        'id': new_page_id,
+        'name': page_name,
+        'path': sdoc_path,
+        'icon': '',
+        'docUuid': str(sdoc_uuid)
+    }
+    pages.append(new_page)
+
+
+def is_sys_path(path):
+    for sys_path in SYS_DIR_PATHS:
+        if path.strip('/').startswith(sys_path):
+            return True
+    return False
+
+
+def md5_repo_id_parent_path(repo_id, parent_path):
+    parent_path = parent_path.rstrip('/') if parent_path != '/' else '/'
+    return hashlib.md5((repo_id + parent_path).encode('utf-8')).hexdigest()
+
+
+def normalize_path(path):
+    return path.rstrip('/') if path != '/' else '/'
+
+
+def create_fileuuidmap_by_uuid(db_session_class, values):
+    with db_session_class() as session:
+        sql = """INSERT INTO tags_fileuuidmap (uuid, repo_id, repo_id_parent_path_md5, parent_path, filename, is_dir)
+                                     VALUES %s""" % ', '.join(
+            ["('%s', '%s', '%s', '%s', '%s', %s)" % value for value in values])
+
+        session.execute(text(sql))
+        session.commit()
+
+
+def generator_base64_code(length=4):
+    possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz0123456789'
+    ids = random.sample(possible, length)
+    return ''.join(ids)
+
+
+def gen_unique_id(id_set, length=4):
+    _id = generator_base64_code(length)
+
+    while True:
+        if _id not in id_set:
+            return _id
+        _id = generator_base64_code(length)
+
+
+def gen_new_page_nav_by_id(navigation, page_id, current_id):
+    new_nav = {
+        'id': page_id,
+        'type': 'page',
+    }
+    if current_id:
+        for nav in navigation:
+            if nav.get('type') == 'page' and nav.get('id') == current_id:
+                sub_nav = nav.get('children', [])
+                sub_nav.append(new_nav)
+                nav['children'] = sub_nav
+                return
+            else:
+                gen_new_page_nav_by_id(nav.get('children', []), page_id, current_id)
+    else:
+        navigation.append(new_nav)
+
+
+def convert_wiki(old_repo_id, new_repo_id, username, db_session_class):
+    new_commit_id = repo_data.get_repo_head_commit(old_repo_id)
+    files = get_diff_files(old_repo_id, ZERO_OBJ_ID, new_commit_id)
+    if not files:
+        return
+    added_files, deleted_files, added_dirs, deleted_dirs, modified_files, renamed_files, moved_files, \
+    renamed_dirs, moved_dirs = files
+
+    navigation = []
+    pages = []
+    page_id_set = set()
+    parent_path_to_page_id = {}
+
+    # first add home.md to new wiki page
+    home_page_path = '/home.md'
+    if new_commit_id is not None:
+        file_id = seafile_api.get_file_id_by_commit_and_path(old_repo_id, new_commit_id, home_page_path)
+    else:
+        file_id = seafile_api.get_file_id_by_path(old_repo_id, home_page_path)
+
+    home_page_content = get_file_content_by_obj_id(old_repo_id, file_id)
+    if home_page_content:
+        home_page_content = md2sdoc(home_page_content, username=username)
+        home_page_content = json.dumps(home_page_content)
+
+    parent_dir = os.path.dirname(home_page_path)
+    base_name = os.path.basename(home_page_path)
+    page_name = os.path.splitext(base_name)[0]
+    create_new_wiki_doc_by_old_wiki_path(page_name, parent_dir, new_repo_id, home_page_content, username,
+                                         parent_path_to_page_id, pages, navigation, page_id_set,
+                                         db_session_class,
+                                         False)
+
+    for repo_dir in added_dirs:
+        path = repo_dir.path
+        if path == '/' or is_sys_path(path):
+            continue
+
+        dir_list = path.strip('/').split('/')
+        pre_dir = '/'
+        for dir1 in dir_list:
+            parent_dir = pre_dir
+            dir_page_name = dir1
+            pre_dir = posixpath.join(pre_dir, dir1)
+            if parent_path_to_page_id.get(pre_dir):
+                continue
+
+            file_content = ''
+            modifier = username
+            old_wiki_path_is_dir = True
+            create_new_wiki_doc_by_old_wiki_path(dir_page_name, parent_dir, new_repo_id, file_content, modifier,
+                                                 parent_path_to_page_id, pages, navigation, page_id_set,
+                                                 db_session_class,
+                                                 old_wiki_path_is_dir)
+
+    for file in added_files:
+        path = file.path.rstrip('/')
+        obj_id = file.obj_id
+
+        if is_sys_path(path):
+            continue
+        file_parent_dir = os.path.dirname(path)
+        file_name = os.path.basename(path)
+        modifier = file.modifier
+
+        file_ext = os.path.splitext(file_name)[1][1:].lower()
+        if path == '/home.md' or file_ext not in ('md', 'markdown'):
+            continue
+
+        markdown_content = get_file_content_by_obj_id(old_repo_id, obj_id)
+        if markdown_content:
+            file_content = md2sdoc(markdown_content, username=username)
+            markdown_content = json.dumps(file_content)
+
+        page_name = os.path.splitext(file_name)[0]
+        create_new_wiki_doc_by_old_wiki_path(page_name, file_parent_dir, new_repo_id, markdown_content, modifier,
+                                             parent_path_to_page_id, pages, navigation, page_id_set,
+                                             db_session_class, False)
+
+    wiki_config = {'version': 1, 'pages': pages, 'navigation': navigation}
+    wiki_config = json.dumps(wiki_config)
+    save_wiki_config(new_repo_id, username, wiki_config)
