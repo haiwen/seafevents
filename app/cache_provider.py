df5f93d53ac1db6ed1daf63e330b401a18ab536a
r350178982 <32759763+r350178982@users.noreply.github.com>
2025-02-26 15:00:22 +0800

    initiate

---
+++ app/cache_provider.py
@@ -0,0 +1,43 @@
+import os
+from seafevents.app.event_redis import redis_cache
+from seafevents.app.memcache import mem_cache
+
+MEMCACHE_PROVIDER = 'memcached'
+REDIS_CACHE_PROVIDER = 'redis'
+
+
+class CacheProvider(object):
+
+    def __init__(self):
+        self.cache_client = None
+        self._init_cache_provider_from_env()
+
+    def _init_cache_provider_from_env(self):
+        cache_provider = os.environ.get('CACHE_PROVIDER')
+        if cache_provider not in [MEMCACHE_PROVIDER, REDIS_CACHE_PROVIDER]:
+            return
+        
+        if cache_provider == MEMCACHE_PROVIDER:
+            self.cache_client = mem_cache
+        else:
+            self.cache_client = redis_cache
+
+    def set(self, key, value, timeout=None):
+        
+        if not self.cache_client:
+            return 
+        return self.cache_client.set(key, value, timeout)
+
+    def get(self, key):
+        
+        if not self.cache_client:
+            return
+        return self.cache_client.get(key)
+    
+    def delete(self, key):
+        
+        if not self.cache_client:
+            return
+        return self.cache_client.delete(key)
+    
+cache = CacheProvider()
--- app/event_redis.py
+++ app/event_redis.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import logging
 
 logger = logging.getLogger(__name__)
@@ -6,34 +7,57 @@ logger = logging.getLogger(__name__)
 
 class RedisClient(object):
 
-    def __init__(self, config, socket_connect_timeout=30, socket_timeout=None):
+    def __init__(self, socket_connect_timeout=30, socket_timeout=None):
         self._host = '127.0.0.1'
         self._port = 6379
         self._password = None
         self.connection = None
 
-        self._parse_config(config, socket_connect_timeout, socket_timeout)
+        self._init_config_from_env(socket_connect_timeout, socket_timeout)
+    
+    def _init_config_from_env(self, socket_connect_timeout, socket_timeout):
+        r_host = os.environ.get('REDIS_SERVER')
+        if r_host:
+            self._host = r_host
+        r_port = os.environ.get('REDIS_PORT')
+        if r_port:
+            self._port = r_port
+        r_password = os.environ.get("REDIS_PASSWORD")
+        if r_password:
+            self._password = r_password
 
-    def _parse_config(self, config, socket_connect_timeout, socket_timeout):
-
-        if not config.has_section('REDIS'):
-            return
-
-        if config.has_option('REDIS', 'server'):
-            self._host = config.get('REDIS', 'server')
-
-        if config.has_option('REDIS', 'port'):
-            self._port = config.getint('REDIS', 'port')
-
-        if config.has_option('REDIS', 'password'):
-            self._password = config.get('REDIS', 'password')
-
-        """
-        By default, each Redis instance created will in turn create its own connection pool.
-        Every caller using redis client will has it's own pool with config caller passed.
-        """
         import redis
         self.connection = redis.Redis(
             host=self._host, port=self._port, password=self._password, decode_responses=True,
             socket_timeout=socket_timeout, socket_connect_timeout=socket_connect_timeout,
         )
+
+    def get(self, key):
+        return self.connection.get(key)
+
+    def set(self, key, value, timeout=None):
+        if not timeout:
+            return self.connection.set(key, value)
+        else:
+            return self.connection.setex(key, timeout, value)
+
+    def delete(self, key):
+        return self.connection.delete(key)
+
+class RedisCache(object):
+
+    CACHE_NAME = 'redis'
+
+    def __init__(self):
+        self._redis_client = RedisClient()
+        
+    def get(self, key):
+        return self._redis_client.get(key)
+
+    def set(self, key, value, timeout=None):
+        return self._redis_client.set(key, value, timeout=timeout)
+
+    def delete(self, key):
+        return self._redis_client.delete(key)
+    
+redis_cache = RedisCache()
