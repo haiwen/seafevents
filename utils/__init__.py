3604fb359183c2102df41733281227972de27368
r350178982 <32759763+r350178982@users.noreply.github.com>
2025-03-05 11:04:50 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -162,7 +162,7 @@ def get_opt_from_conf_or_env(config, section, key, env_key=None, default=None):
         else:
             return os.environ.get(env_key.upper(), default)
 
-        
+
 def parse_bool(v):
     if isinstance(v, bool):
         return v
