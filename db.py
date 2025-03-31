6bbaad6987cb640534a465e885f5c910ac949d48
Junxiang Huang <wacmkxiaoyi@gmail.com>
2025-03-31 11:25:38 +0800

    fix(db.py): variables undefine

--- db.py
+++ db.py
@@ -38,6 +38,11 @@ def create_engine_from_conf(config, db='seafevent'):
         db_name = 'db_name'
 
     db_url = ''
+    host = ''
+    port = ''
+    username = ''
+    passwd = ''
+    dbname = ''
 
     if config.has_section('database'):
         backend = config.get(db_sec, 'type')
