a4e1a352618f49051d940474c03255e293b8ba77
Junxiang Huang <wacmkxiaoyi@gmail.com>
2025-05-24 17:05:28 +0800

    perf(seafile_db.py): default seafile db name to seafile_db

--- utils/seafile_db.py
+++ utils/seafile_db.py
@@ -22,12 +22,12 @@ def get_seafile_db_name():
     config.read(seafile_conf_path)
 
     if config.has_section('database'):
-        db_name = config.get('database', 'db_name', fallback='seafile')
+        db_name = config.get('database', 'db_name', fallback='seafile_db')
         if config.has_option('database', 'type') and config.get('database', 'type') != 'mysql':
             error_msg = 'Failed to init seafile db, only mysql db supported.'
             return None, error_msg
     else:
-        db_name = 'seafile'
+        db_name = 'seafile_db'
     return db_name, None
 
 
@@ -66,7 +66,7 @@ class SeafileDB(object):
             logger.warning('Failed to init seafile db, only mysql db supported.')
             return
 
-        db_name = os.environ.get('SEAFILE_MYSQL_DB_SEAFILE_DB_NAME', '') or seafile_config.get('database', 'db_name', fallback='seafile')
+        db_name = os.environ.get('SEAFILE_MYSQL_DB_SEAFILE_DB_NAME', '') or seafile_config.get('database', 'db_name', fallback='seafile_db')
         db_host = os.getenv('SEAFILE_MYSQL_DB_HOST') or seafile_config.get('database', 'host', fallback='127.0.0.1')
         db_port = int(os.getenv('SEAFILE_MYSQL_DB_PORT', 0)) or seafile_config.getint('database', 'port', fallback=3306)
         db_user = os.getenv('SEAFILE_MYSQL_DB_USER') or seafile_config.get('database', 'user')
