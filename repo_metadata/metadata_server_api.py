5b70910a4feb3e3706bcdb0e29ca19c8c05c5d17
杨国璇 <ygx@MacBookPro.lan>
2025-06-06 13:26:59 +0800

    remove: ocr

--- repo_metadata/metadata_server_api.py
+++ repo_metadata/metadata_server_api.py
@@ -31,6 +31,14 @@ class MetadataServerAPI:
         token = jwt.encode(payload, self.secret_key, algorithm='HS256')
         return {"Authorization": "Bearer %s" % token}
 
+    # metadata
+    def get_metadata(self, base_id):
+        headers = self.gen_headers(base_id)
+        url = f'{METADATA_SERVER_URL}/api/v1/base/{base_id}/metadata'
+        response = requests.get(url, headers=headers, timeout=self.timeout)
+        return parse_response(response)
+
+    # base
     def create_base(self, base_id):
         headers = self.gen_headers(base_id)
         url = f'{self.server_url}/api/v1/base/{base_id}'
@@ -45,7 +53,8 @@ class MetadataServerAPI:
         if response.status_code == 404:
             return {'success': True}
         return parse_response(response)
-    
+
+    # table 
     def create_table(self, base_id, table_name):
         headers = self.gen_headers(base_id)
         url = f'{METADATA_SERVER_URL}/api/v1/base/{base_id}/tables'
@@ -55,16 +64,7 @@ class MetadataServerAPI:
         response = requests.post(url, json=data, headers=headers, timeout=self.timeout)
         return parse_response(response)
 
-    def add_column(self, base_id, table_id, column):
-        headers = self.gen_headers(base_id)
-        url = f'{self.server_url}/api/v1/base/{base_id}/columns'
-        data = {
-            'table_id': table_id,
-            'column': column
-        }
-        response = requests.post(url, json=data, headers=headers, timeout=self.timeout)
-        return parse_response(response)
-
+    # row
     def insert_rows(self, base_id, table_id, rows):
         headers = self.gen_headers(base_id)
         url = f'{self.server_url}/api/v1/base/{base_id}/rows'
@@ -107,6 +107,17 @@ class MetadataServerAPI:
         response = requests.post(url, json=post_data, headers=headers, timeout=self.timeout)
         return parse_response(response)
 
+    # column
+    def add_column(self, base_id, table_id, column):
+        headers = self.gen_headers(base_id)
+        url = f'{self.server_url}/api/v1/base/{base_id}/columns'
+        data = {
+            'table_id': table_id,
+            'column': column
+        }
+        response = requests.post(url, json=data, headers=headers, timeout=self.timeout)
+        return parse_response(response)
+
     def add_columns(self, base_id, table_id, columns):
         headers = self.gen_headers(base_id)
         url = f'{METADATA_SERVER_URL}/api/v1/base/{base_id}/columns'
@@ -126,12 +137,28 @@ class MetadataServerAPI:
         response = requests.get(url, json=data, headers=headers, timeout=self.timeout)
         return parse_response(response)
 
-    def get_metadata(self, base_id):
+    def delete_column(self, base_id, table_id, column_key, permanently=False):
         headers = self.gen_headers(base_id)
-        url = f'{METADATA_SERVER_URL}/api/v1/base/{base_id}/metadata'
-        response = requests.get(url, headers=headers, timeout=self.timeout)
+        url = f'{METADATA_SERVER_URL}/api/v1/base/{base_id}/columns'
+        data = {
+            'table_id': table_id,
+            'column_key': column_key,
+            'permanently': permanently
+        }
+        response = requests.delete(url, json=data, headers=headers, timeout=self.timeout)
         return parse_response(response)
 
+    def update_column(self, base_id, table_id, column):
+        headers = self.gen_headers(base_id)
+        url = f'{METADATA_SERVER_URL}/api/v1/base/{base_id}/columns'
+        data = {
+            'table_id': table_id,
+            'column': column
+        }
+        response = requests.put(url, json=data, headers=headers, timeout=self.timeout)
+        return parse_response(response)
+
+    # link
     def insert_link(self, base_id, link_id, table_id, row_id_map):
         headers = self.gen_headers(base_id)
         url = f'{METADATA_SERVER_URL}/api/v1/base/{base_id}/links'
@@ -154,12 +181,3 @@ class MetadataServerAPI:
         response = requests.put(url, json=data, headers=headers, timeout=self.timeout)
         return parse_response(response)
 
-    def update_column(self, base_id, table_id, column):
-        headers = self.gen_headers(base_id)
-        url = f'{METADATA_SERVER_URL}/api/v1/base/{base_id}/columns'
-        data = {
-            'table_id': table_id,
-            'column': column
-        }
-        response = requests.put(url, json=data, headers=headers, timeout=self.timeout)
-        return parse_response(response)
