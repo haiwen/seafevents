6860fd683d1f71a2985c24ada681a9b99f84d183
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2026-07-07 13:33:16 +0800

    Update change_file_path.py

--- events/change_file_path.py
+++ events/change_file_path.py
@@ -71,8 +71,10 @@ class ChangeFilePathHandler(object):
     def _change_file_uuid_map(self, repo_id, path, new_path, is_dir, src_repo_id = None):
         old_dir = os.path.split(path)[0]
         old_file = os.path.split(path)[1]
-        result = self.session.execute(text('select 1 from tags_fileuuidmap where repo_id=:repo_id and parent_path=:parent_path and filename=:filename and is_dir=:is_dir'),
-                                 {'repo_id': src_repo_id if src_repo_id else repo_id, 'parent_path': old_dir, 'filename': old_file, 'is_dir': is_dir})
+        old_repo_id = src_repo_id if src_repo_id else repo_id
+        old_path_md5 = self.md5_repo_id_parent_path(old_repo_id, old_dir)
+        result = self.session.execute(text('select 1 from tags_fileuuidmap where repo_id_parent_path_md5=:path_md5 and filename=:filename and is_dir=:is_dir'),
+                                 {'path_md5': old_path_md5, 'filename': old_file, 'is_dir': is_dir})
         # update old path itself
         if result.rowcount != 0:
             new_dir = os.path.split(new_path)[0]
@@ -82,9 +84,9 @@ class ChangeFilePathHandler(object):
 
             self.session.execute(text('update tags_fileuuidmap set repo_id=:new_repo_id, parent_path=:new_dir, filename=:new_file, '
                                  'repo_id_parent_path_md5=:new_md5 where '
-                                 'repo_id=:repo_id and parent_path=:dir and filename=:filename and is_dir=:is_dir'),
+                                 'repo_id_parent_path_md5=:old_md5 and filename=:filename and is_dir=:is_dir'),
                             {'new_repo_id': repo_id, 'new_dir': new_dir, 'new_file': new_file, 'new_md5': path_md5,
-                             'repo_id': src_repo_id if src_repo_id else repo_id, 'dir': old_dir, 'filename': old_file, 'is_dir': is_dir})
+                             'old_md5': old_path_md5, 'filename': old_file, 'is_dir': is_dir})
             self.session.commit()
 
         # sub-dir #
@@ -112,12 +114,13 @@ class ChangeFilePathHandler(object):
                     else:
                         new_path_value = new_path + row[0].split(path, 1)[1]
                     path_md5 = self.md5_repo_id_parent_path(repo_id, new_path_value)
+                    old_sub_path_md5 = self.md5_repo_id_parent_path(old_repo_id, row[0])
 
                     self.session.execute(text('update tags_fileuuidmap set repo_id=:new_repo_id, parent_path=:new_dir, '
                                          'repo_id_parent_path_md5=:new_md5 where '
-                                         'repo_id=:repo_id and parent_path=:dir'),
+                                         'repo_id_parent_path_md5=:old_md5 and parent_path=:dir'),
                                     {'new_repo_id': repo_id, 'new_dir': new_path_value, 'new_md5': path_md5,
-                                     'repo_id': src_repo_id if src_repo_id else repo_id, 'dir': row[0]})
+                                     'old_md5': old_sub_path_md5, 'dir': row[0]})
                     self.session.commit()
 
     def md5_repo_id_parent_path(self, repo_id, parent_path):
