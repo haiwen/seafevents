77b2066c97685c49ebdb8f6eea4f3781b1549b8b
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2026-07-22 10:04:04 +0800

    remove init task thread

--- ai_summary/ai_summary.py
+++ ai_summary/ai_summary.py
@@ -7,7 +7,7 @@ from redis.exceptions import ConnectionError as NoMQAvailable, ResponseError, Ti
 from seafevents.mq import get_mq
 from seafevents.ai_summary.ai_summary_manager import AISummaryManager
 from seafevents.ai_summary.ai_summary_worker import AISummaryTaskWorker
-from seafevents.app.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, AI_SUMMARY_INIT_WORKERS, AI_SUMMARY_BATCH_SIZE
+from seafevents.app.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, AI_SUMMARY_WORKERS, AI_SUMMARY_LOCK_BUSY_RETRY_INTERVAL
 
 logger = logging.getLogger('ai_summary')
 
@@ -18,122 +18,12 @@ class AISummary(object):
 
     def __init__(self):
         self.should_stop = threading.Event()
-        self.LOCK_TIMEOUT = 1800
-        self.REFRESH_INTERVAL = 600
-        self.locked_keys = set()
-        self.mq_server = REDIS_HOST
-        self.mq_port = REDIS_PORT
-        self.mq_password = REDIS_PASSWORD
-        self.init_worker_num = 1
-        self.batch_size = 50
-        self._parse_config()
-
-        self.mq = get_mq(self.mq_server, self.mq_port, self.mq_password)
-        self.ai_summary_manager = AISummaryManager()
-        self.ai_summary_task_worker = AISummaryTaskWorker(self.should_stop, self.locked_keys)
-        self.worker_list = []
-
-    def _parse_config(self):
-        self.init_worker_num = AI_SUMMARY_INIT_WORKERS
-        self.batch_size = AI_SUMMARY_BATCH_SIZE
-
-    def _get_init_ai_summary_lock_key(self, repo_id):
-        return 'init_ai_summary_' + repo_id
-
-    @property
-    def tname(self):
-        return threading.current_thread().name
-
-    def clear_worker(self):
-        for th in self.ai_summary_task_worker.worker_list:
-            th.join()
-        for th in self.worker_list:
-            th.join()
-        logger.info('All ai summary worker threads has stopped.')
+        self.ai_summary_task_worker = AISummaryTaskWorker(self.should_stop)
 
     def start(self):
-        if not self.mq:
-            return
-        
         self.ai_summary_task_worker.start()
         logger.info('AI summary task worker started with %d threads', len(self.ai_summary_task_worker.worker_list))
 
-        for i in range(int(self.init_worker_num)):
-            t = threading.Thread(target=self.init_ai_summary_handler, name='init_ai_summary_' + str(i), daemon=True)
-            t.start()
-            self.worker_list.append(t)
-
-        t = threading.Thread(target=self.refresh_lock, name='refresh_thread', daemon=True)
-        t.start()
-        self.worker_list.append(t)
-        self.clear_worker()
-
-    def init_ai_summary_handler(self):
-        logger.info('%s starting init ai summary', self.tname)
-        try:
-            while not self.should_stop.is_set():
-                try:
-                    res = self.mq.brpop('init_ai_summary_task', timeout=30)
-                    if res is not None:
-                        key, value = res
-                        msg = value.split('\t')
-                        if len(msg) != 3:
-                            logger.info('Bad message: %s' % str(msg))
-                        else:
-                            op_type, repo_id, username = msg[0], msg[1], msg[2]
-                            self.ai_summary_task_handler(self.mq, repo_id, self.should_stop, op_type, username)
-                except (ResponseError, NoMQAvailable, TimeoutError) as e:
-                    logger.error('The connection to the redis server failed: %s' % e)
-        except Exception as e:
-            logger.error('%s Handle init ai summary task error' % self.tname)
-            logger.error(e, exc_info=True)
-            time.sleep(0.3)
-
-    def ai_summary_task_handler(self, mq, repo_id, should_stop, op_type, username=None):
-        if not should_stop.is_set():
-            if mq.set(self._get_init_ai_summary_lock_key(repo_id), time.time(), ex=self.LOCK_TIMEOUT, nx=True):
-                logger.info('%s start init ai summary repo %s' % (self.tname, repo_id))
-                lock_key = self._get_init_ai_summary_lock_key(repo_id)
-                self.locked_keys.add(lock_key)
-                self.init_ai_summary(repo_id, username)
-                try:
-                    self.locked_keys.remove(lock_key)
-                except KeyError:
-                    logger.error('%s is already removed. SHOULD NOT HAPPEN!' % lock_key)
-                mq.delete(lock_key)
-                logger.info('%s Finish init ai summary repo: %s, delete redis lock %s' % (self.tname, repo_id, lock_key))
-            else:
-                logger.info('repo: %s init ai summary is running, skip this task', repo_id)
-
-    def init_ai_summary(self, repo_id, username=None):
-        try:
-            self.ai_summary_manager.init_ai_summary(repo_id, username=username, batch_size=self.batch_size)
-        except Exception as e:
-            logger.exception('init ai summary repo: %s, error: %s', repo_id, e)
-
-    def refresh_lock(self):
-        logger.info('%s Starting refresh locks', self.tname)
-        while not self.should_stop.is_set():
-            try:
-                copy = self.locked_keys.copy()
-
-                for lock in copy:
-                    ttl = self.mq.ttl(lock)
-                    new_ttl = ttl + self.REFRESH_INTERVAL
-                    self.mq.expire(lock, new_ttl)
-                    logger.debug('%s Refresh lock [%s] timeout from %s to %s' %
-                                 (self.tname, lock, ttl, new_ttl))
-                time.sleep(self.REFRESH_INTERVAL)
-            except Exception as e:
-                logger.exception(e)
-                time.sleep(1)
-
     def clear(self):
-        if not self.mq:
-            return
         self.should_stop.set()
-        time.sleep(1)
-        for key in self.locked_keys:
-            self.mq.delete(key)
-            logger.info('redis lock key %s has been deleted', key)
         logger.info('Exit ai summary worker')
--- ai_summary/ai_summary_worker.py
+++ ai_summary/ai_summary_worker.py
@@ -7,7 +7,7 @@ from redis.exceptions import ConnectionError as NoMQAvailable, ResponseError, Ti
 
 from seafevents.mq import get_mq
 from seafevents.app.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, SEAFILE_AI_SERVER_URL, SEAFILE_AI_SECRET_KEY, \
-    AI_SUMMARY_WORKERS, AI_SUMMARY_LOCK_BUSY_RETRY_INTERVAL
+    AI_SUMMARY_WORKERS, AI_SUMMARY_LOCK_BUSY_RETRY_INTERVAL, AI_SUMMARY_BATCH_SIZE
 from seafevents.ai_summary.ai_summary_manager import AISummaryManager
 from seafevents.repo_metadata.metadata_server_api import MetadataServerAPI
 from seafevents.repo_metadata.seafile_ai_api import SeafileAIAPI
@@ -19,25 +19,31 @@ logger = logging.getLogger('ai_summary')
 class AISummaryTaskWorker(object):
     HIGH_PRIORITY_TASK_QUEUE = 'ai_summary_task_high'
     LOW_PRIORITY_TASK_QUEUE = 'ai_summary_task_low'
+    INIT_TASK_QUEUE = 'init_ai_summary_task'
 
-    def __init__(self, should_stop, locked_keys):
+    def __init__(self, should_stop):
         self.should_stop = should_stop
-        self.locked_keys = locked_keys
         self.lock_timeout = 1800
+        self.refresh_interval = 600
         self.mq_server = REDIS_HOST
         self.mq_port = REDIS_PORT
         self.mq_password = REDIS_PASSWORD
         self.worker_num = AI_SUMMARY_WORKERS
         self.lock_busy_retry_interval = AI_SUMMARY_LOCK_BUSY_RETRY_INTERVAL
+        self.batch_size = AI_SUMMARY_BATCH_SIZE
 
         self.mq = get_mq(self.mq_server, self.mq_port, self.mq_password)
         self.metadata_server_api = MetadataServerAPI('seafevents')
         self.seafile_ai_api = SeafileAIAPI(SEAFILE_AI_SERVER_URL, SEAFILE_AI_SECRET_KEY)
         self.ai_summary_manager = AISummaryManager()
         self.worker_list = []
+        self.locked_keys = set()
 
     def _get_lock_key(self, repo_id):
         return 'ai_summary_' + repo_id
+
+    def _get_init_lock_key(self, repo_id):
+        return 'init_ai_summary_' + repo_id
 
     def _build_task_payload(self, repo_id, obj_ids, is_init=False):
         return json.dumps({
@@ -62,11 +68,26 @@ class AISummaryTaskWorker(object):
             t.start()
             self.worker_list.append(t)
 
+        t = threading.Thread(target=self.refresh_lock, name='ai_summary_refresh_thread', daemon=True)
+        t.start()
+        self.worker_list.append(t)
+
     def worker_handler(self):
         logger.info('%s starting ai summary worker', self.tname)
         try:
             while not self.should_stop.is_set():
                 try:
+                    init_res = self.mq.brpop(self.INIT_TASK_QUEUE, timeout=1)
+                    if init_res is not None:
+                        key, value = init_res
+                        msg = value.split('\t')
+                        if len(msg) != 3:
+                            logger.info('Bad message: %s' % str(msg))
+                        else:
+                            op_type, repo_id, username = msg[0], msg[1], msg[2]
+                            self.handle_init_task(repo_id, username)
+                        continue
+
                     res = self.mq.brpop([self.HIGH_PRIORITY_TASK_QUEUE, self.LOW_PRIORITY_TASK_QUEUE], timeout=30)
                     if res is None:
                         continue
@@ -128,3 +149,43 @@ class AISummaryTaskWorker(object):
                 logger.error('%s is already removed. SHOULD NOT HAPPEN!', lock_key)
             self.mq.delete(lock_key)
             logger.debug('%s released ai summary lock repo=%s, lock_key=%s', self.tname, repo_id, lock_key)
+
+    def handle_init_task(self, repo_id, username=None):
+        if not repo_id or self.should_stop.is_set():
+            return
+
+        init_lock_key = self._get_init_lock_key(repo_id)
+        if not self.mq.set(init_lock_key, time.time(), ex=self.lock_timeout, nx=True):
+            logger.info('repo: %s init ai summary is running, skip this task', repo_id)
+            return
+
+        self.locked_keys.add(init_lock_key)
+        try:
+            logger.info('%s start init ai summary repo %s', self.tname, repo_id)
+            self.ai_summary_manager.init_ai_summary(repo_id, username=username, batch_size=self.batch_size)
+            logger.info('%s Finish init ai summary repo: %s, delete redis lock %s', self.tname, repo_id, init_lock_key)
+        except Exception as e:
+            logger.exception('init ai summary repo: %s, error: %s', repo_id, e)
+        finally:
+            try:
+                self.locked_keys.remove(init_lock_key)
+            except KeyError:
+                logger.error('%s is already removed. SHOULD NOT HAPPEN!', init_lock_key)
+            self.mq.delete(init_lock_key)
+
+    def refresh_lock(self):
+        logger.info('%s Starting refresh locks', self.tname)
+        while not self.should_stop.is_set():
+            try:
+                copy = self.locked_keys.copy()
+
+                for lock in copy:
+                    ttl = self.mq.ttl(lock)
+                    new_ttl = ttl + self.refresh_interval
+                    self.mq.expire(lock, new_ttl)
+                    logger.debug('%s Refresh lock [%s] timeout from %s to %s' %
+                                 (self.tname, lock, ttl, new_ttl))
+                time.sleep(self.refresh_interval)
+            except Exception as e:
+                logger.exception(e)
+                time.sleep(1)
